81cd8051606ed734ea607cb2b92b9967b929fdc5
CollinGomillia <83978118+CollinGomillia@users.noreply.github.com>
2021-12-11 08:52:54 -0500

    Update DevTeamRepo.cs

--- KomodoInsurance.REPO/DevTeamRepo.cs
+++ KomodoInsurance.REPO/DevTeamRepo.cs
@@ -7,9 +7,50 @@ using KomodoInsurance.POCO;
 
 namespace KomodoInsurance.REPO
 {
-    private readonly List<DevTeam> _komodoTeams = new List<DevTeam>();
-    public class 
+    public class DevTeamRepo
     {
-        private readonly List<DevTeam>
+        private readonly List<DevTeam> _teams = new List<DevTeam>();
+        private readonly List<Developer> _developers = new List<Developer>();
+        private int _count = 0;
+        public bool AddTeams(DevTeam teams)
+        {
+            if (_teams == null)
+            {
+                return false;
+            }
+            
+            _teams.Add(teams);
+            return true;
+
+
+        }
+            
+        public bool RemoveTeams(int id)
+        {
+            DevTeam deleteTeam = GetDevTeamById(id);
+            if (deleteTeam == null)
+            {
+                return false;
+            }
+            else
+            {
+                _teams.Remove(deleteTeam);
+                return true;
+            }
+        }
+
+        public DevTeam GetDevTeamById(int Id)
+        {
+
+
+            foreach (DevTeam devTeams in _teams)
+            {
+                if (Id == devTeams.Id)
+                {
+                    return devTeams;
+                }
+            }
+            return null;
+        }
     }
 }
